87db54fd59502ecf283511695b405a5731f00a44
bkonczyk <bartkon@gmail.com>
2021-08-28 13:30:20 +0200

    building add employee form

--- src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeController.java
+++ src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeController.java
@@ -1,7 +1,29 @@
 package pl.sda.thymeleaf.domain.employee;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        List<Employee> employees = service.findAll();
+        model.addAttribute("employees", employees);
+        return "index";
+    }
+
+    @GetMapping("/add-employee-form")
+    public String addEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "add-employee-form";
+    }
 }
--- src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeService.java
+++ src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeService.java
@@ -1,7 +1,17 @@
 package pl.sda.thymeleaf.domain.employee;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    public List<Employee> findAll() {
+        return repository.findAll();
+    }
 }
