39e561a3b087bea7f1951cf5dd2cb8a5cfc93f44
bkonczyk <bartkon@gmail.com>
2021-08-28 15:44:26 +0200

    working on pagination

--- src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeController.java
+++ src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeController.java
@@ -1,6 +1,7 @@
 package pl.sda.thymeleaf.domain.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,9 +26,10 @@ public class EmployeeController {
 
     @GetMapping("/index")
     public String index(Model model) {
-        List<Employee> employees = service.findAll();
-        model.addAttribute("employees", employees);
-        return "index";
+//        List<Employee> employees = service.findAll();
+//        model.addAttribute("employees", employees);
+//        return "index";
+        return findPage(1, model);
     }
 
     @GetMapping("/add-employee-form")
@@ -50,5 +52,16 @@ public class EmployeeController {
     public String deleteEmployee(@PathVariable UUID id) {
         service.deleteEmployee(id);
         return "redirect:/index";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPage(@PathVariable int pageNo, Model model) {
+        Page<Employee> employeePage = service.findPage(pageNo);
+
+        model.addAttribute("employees", employeePage.getContent());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", employeePage.getTotalPages());
+        model.addAttribute("totalItems", employeePage.getTotalElements());
+        return "index";
     }
 }
--- src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeService.java
+++ src/main/java/pl/sda/thymeleaf/domain/employee/EmployeeService.java
@@ -1,6 +1,9 @@
 package pl.sda.thymeleaf.domain.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +15,9 @@ public class EmployeeService {
 
     private final EmployeeRepository repository;
 
+    @Value("${pageSize}")
+    private int pageSize;
+
     public List<Employee> findAll() {
         return repository.findAll();
     }
@@ -22,5 +28,10 @@ public class EmployeeService {
 
     public void deleteEmployee(UUID id) {
         repository.deleteById(id);
+    }
+
+    public Page<Employee> findPage(int pageNo) {
+        PageRequest pageRequest = PageRequest.of(--pageNo, pageSize);
+        return repository.findAll(pageRequest);
     }
 }
